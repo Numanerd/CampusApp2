94a35ddf0fae3a5b90f83411f90c1078a9ce66a4
keremdizdar <keremdizdar@gmail.com>
2023-06-10 22:53:11 +0300

    test123

--- src/test/java/StepDefinitions/test123.java
+++ src/test/java/StepDefinitions/test123.java
@@ -3,5 +3,6 @@ package StepDefinitions;
 public class test123 {
     public static void main(String[] args) {
         System.out.println("test");
+        System.out.println("test12345");
     }
 }
