07bc7dc3843b32b0f30966eb7734b18eab99980d
SevdaNurHos <sevdanurhos@gmail.com>
2023-06-10 23:18:00 +0300

    deneme1

--- src/test/java/StepDefinitions/Test1.java
+++ src/test/java/StepDefinitions/Test1.java
@@ -3,6 +3,7 @@ package StepDefinitions;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("test yapıldı");
+        System.out.println("test 2");
 
     }
 }
