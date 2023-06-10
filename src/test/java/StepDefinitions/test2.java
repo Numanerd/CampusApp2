57dd1089bf6cbb9d0f3cce19b1ad2bcbe0b22ead
Msı <halilyalcin0732@hotmail.com>
2023-06-10 23:09:38 +0300

    halil3

--- src/test/java/StepDefinitions/test2.java
+++ src/test/java/StepDefinitions/test2.java
@@ -4,5 +4,6 @@ public class test2 {
     public static void main(String[] args) {
         System.out.println("halil");
         System.out.println("asdjsa");
+        System.out.println("xzcxz");
     }
 }
